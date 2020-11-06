1093389159599621b371ea9696d24cded420b7a7
Jmanuelhk <jesusmanuelhk@gmail.com>
2020-11-06 12:31:58 -0600

    array temporadas

--- js/load-page.js
+++ js/load-page.js
@@ -1,22 +1,49 @@
 var promoArray = [
     {
-        "img-prod":"../img/productos/SEVILLANO-NEGRO-PRINCIPAL.jpg",
-        "title-prod":"SEVILLANO WALTON",
-        "desc-prod":"Sombrero 100% en lana Walton (rígida), tafilete de vinyl y forro bordado TARDAN.",
-        "price-prod":"$1,405.00",
+        "temporada" :  "halloween",
+        "color": "orange",
+        "lista_peliculas" : [
+            {
+                "img-prod": "../img/productos/SEVILLANO-NEGRO-PRINCIPAL.jpg",
+                "title-prod": "SEVILLANO WALTON",
+                "desc-prod": "Sombrero 100% en lana Walton (rígida), tafilete de vinyl y forro bordado TARDAN.",
+                "price-prod": "$1,405.00",
+            },
+            {
+                "img-prod": "../img/productos/SEVILLANO-NEGRO-PRINCIPAL.jpg",
+                "title-prod": "SEVILLANO WALTON",
+                "desc-prod": "Sombrero 100% en lana Walton (rígida), tafilete de vinyl y forro bordado TARDAN.",
+                "price-prod": "$1,405.00",
+            },
+            {
+                "img-prod": "../img/productos/SEVILLANO-NEGRO-PRINCIPAL.jpg",
+                "title-prod": "SEVILLANO WALTON",
+                "desc-prod": "Sombrero 100% en lana Walton (rígida), tafilete de vinyl y forro bordado TARDAN.",
+                "price-prod": "$1,405.00",
+            },
+
+        ]
     },
     {
-        "img-prod":"../img/productos/SAFARI-MALLA.jpg",
-        "title-prod":"SAFARI MALLA",
-        "desc-prod":"Sombrero en gabardina de algodón con copa en malla de poliéster rígida para ventilar la cabeza en climas calurosos.",
-        "price-prod":"$1,165.00",
-    },
-    {
-        "img-prod":"../img/productos/PARIS-LANA-AZUL.jpg",
-        "title-prod":"PARIS LANA GRANO",
-        "desc-prod":"Boina clásica en lana cardada. Forro 100% acetato y cinta 100% poliéster. Diseño de palmita en azul.",
-        "price-prod":"$575.00",
-    },
+        "temporada":  "navidad",
+        "color": "green",
+        "lista_peliculas" : [
+
+            {
+                "img-prod": "../img/productos/SAFARI-MALLA.jpg",
+                "title-prod": "SAFARI MALLA",
+                "desc-prod": "Sombrero en gabardina de algodón con copa en malla de poliéster rígida para ventilar la cabeza en climas calurosos.",
+                "price-prod": "$1,165.00",
+            },
+            {
+                "img-prod": "../img/productos/SAFARI-MALLA.jpg",
+                "title-prod": "SAFARI MALLA",
+                "desc-prod": "Sombrero en gabardina de algodón con copa en malla de poliéster rígida para ventilar la cabeza en climas calurosos.",
+                "price-prod": "$1,165.00",
+            },
+
+        ]
+    }
 ]
 
 $(function () {
@@ -25,19 +52,27 @@ $(function () {
     // console.log(rowData);
 
     var newCard = '';
+    var temporada = "navidad"
     
     $.each(promoArray, function (key, element) { 
-        newCard +='<div class="col-sm-4">'
-        newCard +='<div class="card">'
-        newCard += `<img class="card-img-top" src=${element["img-prod"]} alt="">`
-        newCard +='<div class="card-body">'
-        newCard += `<h3 class="card-title">${element["title-prod"]}</h3>`
-        newCard += `<h5 class="card-title">${element["price-prod"]}</h5>`
-        newCard += `<p class="card-text">${element["desc-prod"]}</p>`
-        newCard +='</div>'
-        newCard +='</div>'
-        newCard +='</div>'
-        newCard +='</div>'
+        if (element.temporada === temporada){
+            $("p#tempo").html(element.temporada)
+            $("body").css("background-color", element.color)
+            $.each(element.lista_peliculas, function (key, peli) {
+                newCard += '<div class="col-sm-4">'
+                newCard += '<div class="card">'
+                newCard += `<img class="card-img-top" src=${peli["img-prod"]} alt="">`
+                newCard += '<div class="card-body">'
+                newCard += `<h3 class="card-title">${peli["title-prod"]}</h3>`
+                newCard += `<h5 class="card-title">${peli["price-prod"]}</h5>`
+                newCard += `<p class="card-text">${peli["desc-prod"]}</p>`
+                newCard += '</div>'
+                newCard += '</div>'
+                newCard += '</div>'
+                newCard += '</div>'
+            })
+            
+        }
 
     });
     $('#show-data').append(newCard); 
